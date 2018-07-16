3ce0a45d54d5b43aad48413e29de03003ef7883c
Faustino Martinez <faustmtz@protonmail.com>
2018-07-16 13:10:42 -0700

    Balance on home & expense form started

--- app.js
+++ app.js
@@ -3,13 +3,44 @@ const app = express();
 const http = require('http').Server(app);
 const port = 3000;
 const db = require('./db.js');
-
+var bodyParser = require('body-parser');
 
 app.set('view engine', 'ejs');
 
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+
+function isEmpty(obj) {
+    for(var key in obj) {
+        if(obj.hasOwnProperty(key))
+            return false;
+    }
+    return true;
+}
+
 app.get('/', function(req, res){
-	res.render('home');
-	console.log('you are home');
+	//console.log('you are home');
+	db.Balance.find(function(err, balance){
+		if (err) throw err;
+		if (!isEmpty(balance)){
+			//console.log(balance);
+			res.render('home', {balance: balance});
+		}
+		else
+			res.render('addBalance');
+	});
+});
+
+app.post('/', function(req, res){
+	//console.log(req.body);
+	var balance = new db.Balance({
+		total: req.body.balance
+	});
+
+	balance.save(function(err){
+		if(err) throw (err);
+		res.redirect('/');
+	});
 });
 
 http.listen(port, function(){
